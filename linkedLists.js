12f4fe04ce3ce2ec56a93d28e0fbb5474f2f4029
nemostatus <mattcorralejo1@gmail.com>
2021-02-27 23:42:23 -0800

    created new method to insert first node in list

--- linkedLists.js
+++ linkedLists.js
@@ -4,6 +4,11 @@ class LinkedLists{
         this.head = null //we dont have first yet so null
         this.length = 0 //starts of as 0
     }
+    insertAtHead(data){
+ const newNode = new LinkedListNode(data, this.head)
+ this.head = newNode 
+ length++
+    } //inserts into beginning of list
 }
 
 class LinkedListNode{
